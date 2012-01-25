7c1c96e8ecbf818c609140b217f2bfcc47aa3140
Arnau Sanchez <pyarnau@gmail.com>
2012-01-25 11:13:15 +0100

    setup guard-spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,26 @@
+require 'spork'
 ENV["RAILS_ENV"] ||= 'test'
 
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'capybara/rspec'
-require 'capybara/rails'
-require 'factory_girl'
+Spork.prefork do
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'capybara/rspec'
+  require 'capybara/rails'
+  require 'factory_girl'
 
-Capybara.javascript_driver = :webkit #:webkit | :selenium
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  Capybara.javascript_driver = :webkit #:webkit | :selenium
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-RSpec.configure do |config|
-  config.mock_with :rspec # :spec | :mocha | :flexmock | :rr
-  config.use_transactional_fixtures = true
-  config.include Devise::TestHelpers, :type => :controller
+  RSpec.configure do |config|
+    config.mock_with :rspec # :spec | :mocha | :flexmock | :rr
+    config.use_transactional_fixtures = true
+    config.include Devise::TestHelpers, :type => :controller
+  end
+end
+
+Spork.each_run do
+  load "#{Rails.root}/config/routes.rb"
+  Dir["#{Rails.root}/app/**/*.rb"].each {|f| load f}
+  FactoryGirl.reload
+  #ActiveSupport::Dependencies.clear  
 end
